7cd274f6eff65a5f6c4daf4198a390f7978a822e
Rachel Sharp <rachel@hashicorp.com>
2021-10-08 11:52:29 -0400

    Revert "boilerplate"
    This reverts commit f8f5faf6cad95625e0ebbfaac3fc5ce2e5562bbd.

--- examples/main.tf
+++ examples/main.tf
@@ -11,9 +11,30 @@ provider "hashicups" {
   host     = "http://localhost:19090"
 }
 
-data "hashicups_coffees" "all" {}
+resource "hashicups_order" "edu" {
+  items = [{
+    coffee = {
+      id = 3
+    }
+    quantity = 2
+    }, {
+    coffee = {
+      id = 1
+    }
+    quantity = 2
+    }
+  ]
+}
 
-# Returns all coffees
-output "all_coffees" {
-  value = data.hashicups_coffees.all.coffees
+data "hashicups_order" "order" {
+  id = 2
+}
+
+output "order" {
+  value = data.hashicups_order.order
+}
+
+
+output "edu_order" {
+  value = hashicups_order.edu
 }
--- hashicups/provider.go
+++ hashicups/provider.go
@@ -157,12 +157,15 @@ func (p *provider) Configure(ctx context.Context, req tfsdk.ConfigureProviderReq
 
 // GetResources - Defines provider resources
 func (p *provider) GetResources(_ context.Context) (map[string]tfsdk.ResourceType, diag.Diagnostics) {
-	return map[string]tfsdk.ResourceType{}, nil
+	return map[string]tfsdk.ResourceType{
+		"hashicups_order": resourceOrderType{},
+	}, nil
 }
 
 // GetDataSources - Defines provider data sources
 func (p *provider) GetDataSources(_ context.Context) (map[string]tfsdk.DataSourceType, diag.Diagnostics) {
 	return map[string]tfsdk.DataSourceType{
 		"hashicups_coffees": dataSourceCoffeesType{},
+		"hashicups_order":   dataSourceOrderType{},
 	}, nil
 }
