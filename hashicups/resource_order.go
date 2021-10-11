8a198bb2d83b7577545f491691732e48656c13f2
Rachel Sharp <rachel@hashicorp.com>
2021-10-11 18:35:53 -0400

    Update resource_order.go

--- hashicups/resource_order.go
+++ hashicups/resource_order.go
@@ -5,13 +5,68 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 type resourceOrderType struct{}
 
 // Order Resource schema
 func (r resourceOrderType) GetSchema(_ context.Context) (tfsdk.Schema, diag.Diagnostics) {
-	return tfsdk.Schema{}, nil
+	return tfsdk.Schema{
+		Attributes: map[string]tfsdk.Attribute{
+			"id": {
+				Type: types.StringType,
+				// When Computed is true, the provider will set value --
+				// the user cannot define the value
+				Computed: true,
+			},
+			"last_updated": {
+				Type:     types.StringType,
+				Computed: true,
+			},
+			"items": {
+				// If Required is true, Terraform will throw error if user
+				// doesn't specify value
+				// If Optional is true, user can choose to supply a value
+				Required: true,
+				Attributes: tfsdk.ListNestedAttributes(map[string]tfsdk.Attribute{
+					"quantity": {
+						Type:     types.NumberType,
+						Required: true,
+					},
+					"coffee": {
+						Required: true,
+						Attributes: tfsdk.SingleNestedAttributes(map[string]tfsdk.Attribute{
+							"id": {
+								Type:     types.NumberType,
+								Required: true,
+							},
+							"name": {
+								Type:     types.StringType,
+								Computed: true,
+							},
+							"teaser": {
+								Type:     types.StringType,
+								Computed: true,
+							},
+							"description": {
+								Type:     types.StringType,
+								Computed: true,
+							},
+							"price": {
+								Type:     types.NumberType,
+								Computed: true,
+							},
+							"image": {
+								Type:     types.StringType,
+								Computed: true,
+							},
+						}),
+					},
+				}, tfsdk.ListNestedAttributesOptions{}),
+			},
+		},
+	}, nil
 }
 
 // New resource instance
