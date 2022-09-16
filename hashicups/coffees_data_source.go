d9bd6ea5c4e4231b91f41fd70ee9da3bf71123c3
Brian Flad <bflad417@gmail.com>
2022-09-15 21:57:36 -0400

    Implement coffees data source

---
+++ hashicups/coffees_data_source.go
@@ -0,0 +1,149 @@
+package hashicups
+
+import (
+	"context"
+
+	"github.com/hashicorp-demoapp/hashicups-client-go"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+// Ensure the implementation satisfies the expected interfaces.
+var (
+	_ datasource.DataSource              = &coffeesDataSource{}
+	_ datasource.DataSourceWithConfigure = &coffeesDataSource{}
+)
+
+// NewCoffeesDataSource is a helper function to simplify the provider implementation.
+func NewCoffeesDataSource() datasource.DataSource {
+	return &coffeesDataSource{}
+}
+
+// coffeesDataSource is the data source implementation.
+type coffeesDataSource struct {
+	client *hashicups.Client
+}
+
+// coffeesDataSourceModel maps the data source schema data.
+type coffeesDataSourceModel struct {
+	Coffees []coffeesModel `tfsdk:"coffees"`
+}
+
+// coffeesModel maps coffees schema data.
+type coffeesModel struct {
+	ID          types.Int64               `tfsdk:"id"`
+	Name        types.String              `tfsdk:"name"`
+	Teaser      types.String              `tfsdk:"teaser"`
+	Description types.String              `tfsdk:"description"`
+	Price       types.Float64             `tfsdk:"price"`
+	Image       types.String              `tfsdk:"image"`
+	Ingredients []coffeesIngredientsModel `tfsdk:"ingredients"`
+}
+
+// coffeesIngredientsModel maps coffee ingredients data
+type coffeesIngredientsModel struct {
+	ID types.Int64 `tfsdk:"id"`
+}
+
+// Metadata returns the data source type name.
+func (d *coffeesDataSource) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_coffees"
+}
+
+// GetSchema defines the schema for the data source.
+func (d *coffeesDataSource) GetSchema(_ context.Context) (tfsdk.Schema, diag.Diagnostics) {
+	return tfsdk.Schema{
+		Attributes: map[string]tfsdk.Attribute{
+			"coffees": {
+				Computed: true,
+				Attributes: tfsdk.ListNestedAttributes(map[string]tfsdk.Attribute{
+					"id": {
+						Type:     types.Int64Type,
+						Computed: true,
+					},
+					"name": {
+						Type:     types.StringType,
+						Computed: true,
+					},
+					"teaser": {
+						Type:     types.StringType,
+						Computed: true,
+					},
+					"description": {
+						Type:     types.StringType,
+						Computed: true,
+					},
+					"price": {
+						Type:     types.Float64Type,
+						Computed: true,
+					},
+					"image": {
+						Type:     types.StringType,
+						Computed: true,
+					},
+					"ingredients": {
+						Computed: true,
+						Attributes: tfsdk.ListNestedAttributes(map[string]tfsdk.Attribute{
+							"id": {
+								Type:     types.Int64Type,
+								Computed: true,
+							},
+						}),
+					},
+				}),
+			},
+		},
+	}, nil
+}
+
+// Configure adds the provider configured client to the data source.
+func (d *coffeesDataSource) Configure(_ context.Context, req datasource.ConfigureRequest, _ *datasource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	d.client = req.ProviderData.(*hashicups.Client)
+}
+
+// Read refreshes the Terraform state with the latest data.
+func (d *coffeesDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var state coffeesDataSourceModel
+
+	coffees, err := d.client.GetCoffees()
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to Read HashiCups Coffees",
+			err.Error(),
+		)
+		return
+	}
+
+	// Map response body to model
+	for _, coffee := range coffees {
+		coffeeState := coffeesModel{
+			ID:          types.Int64{Value: int64(coffee.ID)},
+			Name:        types.String{Value: coffee.Name},
+			Teaser:      types.String{Value: coffee.Teaser},
+			Description: types.String{Value: coffee.Description},
+			Price:       types.Float64{Value: coffee.Price},
+			Image:       types.String{Value: coffee.Image},
+		}
+
+		for _, ingredient := range coffee.Ingredient {
+			coffeeState.Ingredients = append(coffeeState.Ingredients, coffeesIngredientsModel{
+				ID: types.Int64{Value: int64(ingredient.ID)},
+			})
+		}
+
+		state.Coffees = append(state.Coffees, coffeeState)
+	}
+
+	// Set state
+	diags := resp.State.Set(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
