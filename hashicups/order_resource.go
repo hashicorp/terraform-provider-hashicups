4578e094625040a4cbed5736c0d932738a9be833
Brian Flad <bflad417@gmail.com>
2022-09-19 11:00:01 -0500

    Implement order resource with Create and Read functionality

---
+++ hashicups/order_resource.go
@@ -0,0 +1,228 @@
+package hashicups
+
+import (
+	"context"
+	"strconv"
+	"time"
+
+	"github.com/hashicorp-demoapp/hashicups-client-go"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+// Ensure the implementation satisfies the expected interfaces.
+var (
+	_ resource.Resource              = &orderResource{}
+	_ resource.ResourceWithConfigure = &orderResource{}
+)
+
+// NewOrderResource is a helper function to simplify the provider implementation.
+func NewOrderResource() resource.Resource {
+	return &orderResource{}
+}
+
+// orderResource is the resource implementation.
+type orderResource struct {
+	client *hashicups.Client
+}
+
+// orderResourceModel maps the resource schema data.
+type orderResourceModel struct {
+	ID          types.String     `tfsdk:"id"`
+	Items       []orderItemModel `tfsdk:"items"`
+	LastUpdated types.String     `tfsdk:"last_updated"`
+}
+
+// orderItemModel maps order item data.
+type orderItemModel struct {
+	Coffee   orderItemCoffeeModel `tfsdk:"coffee"`
+	Quantity types.Int64          `tfsdk:"quantity"`
+}
+
+// orderItemCoffeeModel maps coffee order item data.
+type orderItemCoffeeModel struct {
+	ID          types.Int64   `tfsdk:"id"`
+	Name        types.String  `tfsdk:"name"`
+	Teaser      types.String  `tfsdk:"teaser"`
+	Description types.String  `tfsdk:"description"`
+	Price       types.Float64 `tfsdk:"price"`
+	Image       types.String  `tfsdk:"image"`
+}
+
+// Metadata returns the data source type name.
+func (r *orderResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_order"
+}
+
+// GetSchema defines the schema for the data source.
+func (r *orderResource) GetSchema(_ context.Context) (tfsdk.Schema, diag.Diagnostics) {
+	return tfsdk.Schema{
+		Attributes: map[string]tfsdk.Attribute{
+			"id": {
+				Type:     types.StringType,
+				Computed: true,
+			},
+			"last_updated": {
+				Type:     types.StringType,
+				Computed: true,
+			},
+			"items": {
+				Required: true,
+				Attributes: tfsdk.ListNestedAttributes(map[string]tfsdk.Attribute{
+					"quantity": {
+						Type:     types.Int64Type,
+						Required: true,
+					},
+					"coffee": {
+						Required: true,
+						Attributes: tfsdk.SingleNestedAttributes(map[string]tfsdk.Attribute{
+							"id": {
+								Type:     types.Int64Type,
+								Required: true,
+							},
+							"name": {
+								Type:     types.StringType,
+								Computed: true,
+							},
+							"teaser": {
+								Type:     types.StringType,
+								Computed: true,
+							},
+							"description": {
+								Type:     types.StringType,
+								Computed: true,
+							},
+							"price": {
+								Type:     types.Float64Type,
+								Computed: true,
+							},
+							"image": {
+								Type:     types.StringType,
+								Computed: true,
+							},
+						}),
+					},
+				}),
+			},
+		},
+	}, nil
+}
+
+// Configure adds the provider configured client to the data source.
+func (r *orderResource) Configure(_ context.Context, req resource.ConfigureRequest, _ *resource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	r.client = req.ProviderData.(*hashicups.Client)
+}
+
+// Create creates the resource and sets the initial Terraform state.
+func (r *orderResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	// Retrieve values from plan
+	var plan orderResourceModel
+	diags := req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Generate API request body from plan
+	var items []hashicups.OrderItem
+	for _, item := range plan.Items {
+		items = append(items, hashicups.OrderItem{
+			Coffee: hashicups.Coffee{
+				ID: int(item.Coffee.ID.Value),
+			},
+			Quantity: int(item.Quantity.Value),
+		})
+	}
+
+	// Create new order
+	order, err := r.client.CreateOrder(items)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error creating order",
+			"Could not create order, unexpected error: "+err.Error(),
+		)
+		return
+	}
+
+	// Map response body to schema and populate Computed attribute values
+	plan.ID = types.String{Value: strconv.Itoa(order.ID)}
+	for itemIndex, item := range order.Items {
+		plan.Items[itemIndex] = orderItemModel{
+			Coffee: orderItemCoffeeModel{
+				ID:          types.Int64{Value: int64(item.Coffee.ID)},
+				Name:        types.String{Value: item.Coffee.Name},
+				Teaser:      types.String{Value: item.Coffee.Teaser},
+				Description: types.String{Value: item.Coffee.Description},
+				Price:       types.Float64{Value: item.Coffee.Price},
+				Image:       types.String{Value: item.Coffee.Image},
+			},
+			Quantity: types.Int64{Value: int64(item.Quantity)},
+		}
+	}
+	plan.LastUpdated = types.String{Value: string(time.Now().Format(time.RFC850))}
+
+	// Set state to fully populated data
+	diags = resp.State.Set(ctx, plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+// Read refreshes the Terraform state with the latest data.
+func (r *orderResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	// Get current state
+	var state orderResourceModel
+	diags := req.State.Get(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Get refreshed order value from HashiCups
+	order, err := r.client.GetOrder(state.ID.Value)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error Reading HashiCups Order",
+			"Could not read HashiCups order ID "+state.ID.Value+": "+err.Error(),
+		)
+		return
+	}
+
+	// Overwrite items with refreshed state
+	state.Items = []orderItemModel{}
+	for _, item := range order.Items {
+		state.Items = append(state.Items, orderItemModel{
+			Coffee: orderItemCoffeeModel{
+				ID:          types.Int64{Value: int64(item.Coffee.ID)},
+				Name:        types.String{Value: item.Coffee.Name},
+				Teaser:      types.String{Value: item.Coffee.Teaser},
+				Description: types.String{Value: item.Coffee.Description},
+				Price:       types.Float64{Value: item.Coffee.Price},
+				Image:       types.String{Value: item.Coffee.Image},
+			},
+			Quantity: types.Int64{Value: int64(item.Quantity)},
+		})
+	}
+
+	// Set refreshed state
+	diags = resp.State.Set(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+// Update updates the resource and sets the updated Terraform state on success.
+func (r *orderResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+}
+
+// Delete deletes the resource and removes the Terraform state on success.
+func (r *orderResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+}
