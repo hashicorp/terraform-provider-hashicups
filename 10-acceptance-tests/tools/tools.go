015dec88e5a1072b5696d27e9861ab7e388ec729
Bruno Schaatsbergen <git@bschaatsbergen.com>
2024-09-09 19:42:01 +0200

    chore: add terraform fmt to tools

--- 10-acceptance-tests/tools/tools.go
+++ 10-acceptance-tests/tools/tools.go
@@ -10,5 +10,11 @@ import (
 	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
 )
 
+// Run "go generate" to format example terraform files and generate the docs for the registry/website
+
+// If you do not have terraform installed, you can remove the formatting command, but its suggested to
+// ensure the documentation is formatted properly.
+//go:generate terraform fmt -recursive ../examples/
+
 // Generate documentation.
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-dir .. -provider-name hashicups
